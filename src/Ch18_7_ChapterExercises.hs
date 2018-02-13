e8b77664b2599ad05a334d0c0d0d9e903617065f
wafelj <florjan.bartol@gmail.com>
2018-02-13 22:43:45 +0000

    Added PhhhbbtttEither monad instance

--- src/Ch18_7_ChapterExercises.hs
+++ src/Ch18_7_ChapterExercises.hs
@@ -1,5 +1,6 @@
 module Ch18_7_ChapterExercises where
 
+-- 1
 data Nope a =
   NopeDotJpg
   deriving (Eq, Show)
@@ -14,3 +15,24 @@ instance Applicative Nope where
 instance Monad Nope where
   return _ = NopeDotJpg
   _ >>= _  = NopeDotJpg
+
+
+--2
+data PhhhbbtttEither b a =
+    PLeft a
+  | PRight b
+  deriving (Eq, Show)
+
+instance Functor (PhhhbbtttEither b) where 
+  fmap _ (PRight b) = PRight b
+  fmap f (PLeft a) = PLeft $ f a
+
+instance Applicative (PhhhbbtttEither b) where
+  pure a = PLeft a
+  PRight f <*> _ = PRight f
+  PLeft f <*> l = fmap f l
+
+instance Monad (PhhhbbtttEither b) where
+  return = pure
+  PRight b >>= _ = PRight b
+  PLeft a >>= f = f a
--- test/Ch18_7_ChapterExercises_Test.hs
+++ test/Ch18_7_ChapterExercises_Test.hs
@@ -5,10 +5,30 @@ import Test.QuickCheck
 import Test.QuickCheck.Checkers
 import Test.QuickCheck.Classes
 
+
+--1
 instance Arbitrary (Nope a) where
   arbitrary = return NopeDotJpg
 
 instance EqProp (Nope a) where (=-=) = eq
 
+
+--2
+instance (Arbitrary a, Arbitrary b) => Arbitrary (PhhhbbtttEither b a) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    frequency [(1, return $ PLeft a),
+               (1, return $ PRight b)]
+
+instance (Eq a, Eq b) => EqProp (PhhhbbtttEither b a) where (=-=) = eq
+
 test :: IO ()
-test = quickBatch $ monad (NopeDotJpg :: Nope (Int, Int, Int))
+test = do
+  quickBatch $ monad (NopeDotJpg :: Nope (Int, Int, Int))
+  quickBatch $ functor (PLeft ('a', 'b', 'c')
+                            :: PhhhbbtttEither Int (Char, Char, Char))
+  quickBatch $ applicative (PLeft ('a', 'b', 'c')
+                            :: PhhhbbtttEither Int (Char, Char, Char))
+  quickBatch $ monad (PLeft ('a', 'b', 'c')
+                      :: PhhhbbtttEither Int (Char, Char, Char))
