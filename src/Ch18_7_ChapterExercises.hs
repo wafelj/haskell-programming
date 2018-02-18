07b6071c7d00d71d651043f6809e488b1de816e6
wafelj <florjan.bartol@gmail.com>
2018-02-18 21:04:31 +0000

    Added solutions for remaining Monad exercises

--- src/Ch18_7_ChapterExercises.hs
+++ src/Ch18_7_ChapterExercises.hs
@@ -83,3 +83,30 @@ instance Monad List where
   return = pure
   Nil >>= _ = Nil
   l   >>= f = flatten $ fmap f l
+
+
+
+--1
+j :: Monad m => m (m a) -> m a
+j = (>>= id)
+
+--2
+l1 :: Monad m => (a -> b) -> m a -> m b
+l1 = fmap
+
+--3
+l2 :: Monad m => (a -> b -> c) -> m a -> m b -> m c
+l2 f ma mb = f <$> ma <*> mb
+
+--4
+a :: Monad m => m a -> m (a -> b) -> m b
+a = flip (<*>)
+
+--5
+meh :: Monad m => [a] -> (a -> m b) -> m [b]
+meh []     _ = return []
+meh (a:as) f = l2 (:) (f a) (meh as f)
+
+--6
+flipType :: Monad m => [m a] -> m [a]
+flipType a = meh a id
