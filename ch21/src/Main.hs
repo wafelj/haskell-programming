e12f8a6b6fe7eabfa3ad5ca8e617d25abbcb4c77
Florjan Bartol <florjan.bartol@gmail.com>
2018-04-11 22:50:33 +0200

    Added Traversable instance for Tree

--- ch21/src/Main.hs
+++ ch21/src/Main.hs
@@ -204,6 +204,48 @@ instance Traversable n => Traversable (S n) where
   traverse f (S na a) = S <$> traverse f na <*> f a
 
 
+-- Tree
+data Tree a =
+    Empty
+  | Leaf a
+  | Node (Tree a) a (Tree a)
+  deriving (Eq, Show)
+
+instance Functor Tree where
+  fmap _ Empty = Empty
+  fmap f (Leaf a) = Leaf $ f a
+  fmap f (Node t1 a t2) = Node (f <$> t1) (f a) (f <$> t2)
+
+-- foldMap is a bit easier
+-- and looks more natural,
+-- but you can do foldr too
+-- for extra credit.
+instance Foldable Tree where
+  foldMap _ Empty = mempty
+  foldMap f (Leaf a) = f a
+  foldMap f (Node t1 a t2) = foldMap f t1 `mappend` f a `mappend` foldMap f t2
+
+instance Traversable Tree where
+  traverse f Empty = pure Empty
+  traverse f (Leaf a) = Leaf <$> f a
+  traverse f (Node t1 a t2) = Node <$> traverse f t1 <*> f a <*> traverse f t2
+
+instance Arbitrary a => Arbitrary (Tree a) where
+  arbitrary = treeGen
+
+treeGen :: Arbitrary a => Gen (Tree a)
+treeGen = do
+  t1 <- treeGen
+  t2 <- treeGen
+  a  <- arbitrary
+  elements [ Leaf a
+           , Empty
+           , Node t1 a t2
+           ]
+
+instance (Eq a) => EqProp (Tree a) where (=-=) = eq
+
+
 main :: IO ()
 main = do
   let triggerIdentity :: Identity (Int, Int, [Int])
@@ -237,3 +279,6 @@ main = do
   let triggerS :: (S []) (Int, Int, [Int])
       triggerS = undefined
   quickBatch (traversable triggerS)
+
+  let triggerTree = undefined
+  quickBatch $ traversable (triggerTree :: Tree (Int, Int, [Int]))
