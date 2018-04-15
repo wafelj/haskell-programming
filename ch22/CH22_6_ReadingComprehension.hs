d3938ba633c5d203311fd10a34d41eed65f69dec
Florjan Bartol <florjan.bartol@gmail.com>
2018-04-15 23:30:09 +0200

    Added 22_6_3

--- ch22/CH22_6_ReadingComprehension.hs
+++ ch22/CH22_6_ReadingComprehension.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE InstanceSigs #-}
+
+import Control.Monad.Trans.Reader
+
 myLiftA2 :: Applicative f =>
             (a -> b -> c)
          -> f a -> f b -> f c
@@ -5,3 +9,13 @@ myLiftA2 f x y = f <$> x <*> y
 
 asks :: (r -> a) -> Reader r a
 asks = Reader
+
+instance Applicative (Reader r) where
+  pure :: a -> Reader r a
+  pure a = Reader $ const a
+
+  (<*>) :: Reader r (a -> b)
+    -> Reader r a
+    -> Reader r b
+  (Reader rab) <*> (Reader ra) =
+    Reader $ \r -> rab r <$> ra
