5f50d8ad3e19bee26d8bfdcb5c32c78c173a7e05
Florjan Bartol <florjan.bartol@gmail.com>
2018-11-28 06:14:54 +0100

    Implemented simple queue, added sequence benchmark

--- ch28/simple-queue/benchmark/Main.hs
+++ ch28/simple-queue/benchmark/Main.hs
@@ -1,19 +1,31 @@
 import Criterion.Main
 import Data.Maybe
+import Data.Sequence
 import Queue
 
 main :: IO ()
-main = defaultMain [
-    bench "list" (whnf benchList 1000)
-  , bench "queue" (whnf benchQueue 1000)
-  ]
+main = do
+  let reps = 1000
+  defaultMain [
+      bench "list" (nf benchList reps)
+    , bench "queue" (nf benchQueue reps)
+    , bench "seq" (nf benchSequence reps)
+    ]
 
 benchList :: Int -> [Int]
-benchList i = go i [1..5000]
-  where go 0 q = q
-        go n q = go (n-1) (init $ n:q)
+benchList i = go i [1..50000]
+        where go 0 q = q
+              go n q = go (n-1) (init $ n:q)
 
 benchQueue :: Int -> Queue Int
-benchQueue i = go i (Queue [1..2500] [5000,4999..2501])
-  where go 0 q = q
-        go n q = go (n-1) $ snd (fromJust (pop $ push n q))
+benchQueue i = go i (Queue [1..25000] [50000,49999..2501])
+         where go 0 q = q
+               go n q = go (n-1) $ snd (fromJust (pop $ push n q))
+
+benchSequence :: Int -> Seq Int
+benchSequence i = go i (fromList [1..50000])
+            where go 0 s = s
+                  go n s = go (n-1) (seqInit (n <| s))
+                  seqInit s = case viewr s of
+                                EmptyR  -> empty
+                                s' :> _ -> s'
--- ch28/simple-queue/library/Queue.hs
+++ ch28/simple-queue/library/Queue.hs
@@ -1,12 +1,20 @@
 module Queue where
 
+import Control.DeepSeq
+
 data Queue a =
   Queue { enqueue :: [a]
         , dequeue :: [a]
         } deriving (Eq, Show)
 
+instance NFData a => NFData (Queue a) where
+  rnf (Queue enq deq) = rnf enq `seq` rnf deq
+
 push :: a -> Queue a -> Queue a
-push = undefined
+push a (Queue enq deq) = Queue (a:enq) deq
 
 pop :: Queue a -> Maybe (a, Queue a)
-pop = undefined
+pop (Queue []  [])    = Nothing
+pop (Queue enq [])    = Just (a, Queue [] enq')
+                          where (a:enq') = reverse enq
+pop (Queue enq (a:deq)) = Just (a, Queue enq deq)
