afdf44639eeb8ecf82136cda2810826d55dc2614
Florjan Bartol <florjan.bartol@gmail.com>
2018-12-04 08:51:51 +0100

    Implemented main for vigenere package

--- ch29/vigenere/executable/Main.hs
+++ ch29/vigenere/executable/Main.hs
@@ -1,4 +1,24 @@
+import System.Environment (getArgs)
+import System.Exit
+
 import qualified Vigenere
 
+data Mode = ModeEncrypt | ModeDecrypt
+
 main :: IO ()
-main = Vigenere.main
+main = do
+  args <- getArgs
+
+  (mode, keyword) <- 
+    case args of
+      "-e":keyword:_ -> return (ModeEncrypt, keyword)
+      "-d":keyword:_ -> return (ModeDecrypt, keyword)
+      _              -> die "Usage: vigenere (-d|-e)"
+
+  message <- getLine
+  
+  putStrLn $ 
+    (case mode of
+      ModeEncrypt -> Vigenere.encrypt
+      ModeDecrypt -> Vigenere.decrypt
+    ) keyword message
--- ch29/vigenere/library/Vigenere.hs
+++ ch29/vigenere/library/Vigenere.hs
@@ -1,4 +1,4 @@
-module Vigenere (main, vigenere) where
+module Vigenere (encrypt, decrypt) where
 
 import Data.Char
 
@@ -12,11 +12,10 @@ substitute d s c   = chr . denorm . (flip mod 26) . (op d s) . norm . ord $ c
           op RightDir = (+)
           op LeftDir = (-)
 
-vigenere :: String -> String -> String
-vigenere ks xs = map f pairs
+encrypt :: String -> String -> String
+encrypt keyword message = map f pairs
   where f (x, k) = substitute RightDir (getOffset k) x
-        pairs = zip xs (concat . repeat $ ks)
+        pairs = zip message (concat . repeat $ keyword)
         getOffset k = mod (ord k) (ord 'A')
 
-main :: IO ()
-main = putStrLn "vigenere"
+decrypt = undefined
