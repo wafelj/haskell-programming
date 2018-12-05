7f8860f6d2e08e4889b3fc18c73bef059332280b
Florjan Bartol <florjan.bartol@gmail.com>
2018-12-05 06:50:20 +0100

    Implemented decrypt function and refactored vigenere code

--- ch29/vigenere/library/Vigenere.hs
+++ ch29/vigenere/library/Vigenere.hs
@@ -2,20 +2,37 @@ module Vigenere (encrypt, decrypt) where
 
 import Data.Char
 
-data Direction = LeftDir | RightDir deriving Show
-
-substitute :: Direction -> Int -> Char -> Char
-substitute _ _ ' ' = ' '
-substitute d s c   = chr . denorm . (flip mod 26) . (op d s) . norm . ord $ c
-    where norm   = flip mod (ord 'A')
-          denorm = (+ ord 'A')
-          op RightDir = (+)
-          op LeftDir = (-)
-
 encrypt :: String -> String -> String
-encrypt keyword message = map f pairs
-  where f (x, k) = substitute RightDir (getOffset k) x
-        pairs = zip message (concat . repeat $ keyword)
-        getOffset k = mod (ord k) (ord 'A')
+encrypt keyword message = 
+  map encryptChar $ constructPairs message keyword
 
-decrypt = undefined
+decrypt :: String -> String -> String
+decrypt keyword message =
+  map decryptChar $ constructPairs message keyword
+
+constructPairs :: String -> String -> [(Char, Char)]
+constructPairs message keyword = 
+  zip message (concat . repeat $ keyword)
+
+encryptChar :: (Char, Char) -> Char
+encryptChar = substituteChar (+)
+
+decryptChar :: (Char, Char) -> Char
+decryptChar = substituteChar (-)
+
+substituteChar :: (Int -> Int -> Int) -> (Char, Char) -> Char
+substituteChar op (c, k) = 
+  intToChar $ 
+  (charToInt c `op` charToInt k) `mod` alphabetLength
+
+alphabetLength :: Int
+alphabetLength = 26
+
+charToInt :: Char -> Int
+charToInt c = mod (ord c) (ord firstChar)
+
+intToChar :: Int -> Char
+intToChar i = chr $ i + ord firstChar
+
+firstChar :: Char
+firstChar = 'A'
