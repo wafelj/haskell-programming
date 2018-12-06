f7a26dbb1a918353256056dc19c6ae76a4d690fd
Florjan Bartol <florjan.bartol@gmail.com>
2018-12-06 06:40:03 +0100

    Added timeout feature to vigenere

--- ch29/vigenere/executable/Main.hs
+++ ch29/vigenere/executable/Main.hs
@@ -1,19 +1,40 @@
 import System.Environment (getArgs)
 import System.Exit
+import System.IO
+
+import Control.Monad
+import Data.Maybe
+import Text.Read
 
 import qualified Vigenere
 
 data Mode = ModeEncrypt | ModeDecrypt
 
+defaultTimeout :: Int
+defaultTimeout = 10000
+
 main :: IO ()
 main = do
   args <- getArgs
 
-  (mode, keyword) <- 
+  (mode, timeout, keyword) <- 
     case args of
-      "-e":keyword:_ -> return (ModeEncrypt, keyword)
-      "-d":keyword:_ -> return (ModeDecrypt, keyword)
-      _              -> die "Usage: vigenere (-d|-e)"
+      "-e":"-t":timeoutStr:keyword:[] -> do
+        timeout <- parseTimeout timeoutStr
+        return (ModeEncrypt, timeout, keyword)
+      "-d":"-t":timeoutStr:keyword:[] -> do
+        timeout <- parseTimeout timeoutStr
+        return (ModeDecrypt, timeout, keyword)
+      "-e":keyword:[] ->
+        return (ModeEncrypt, defaultTimeout, keyword)
+      "-d":keyword:[] ->
+        return (ModeDecrypt, defaultTimeout, keyword)
+      _ ->
+        die "Usage: vigenere (-d|-e) [-t miliseconds] keyword"
+
+  inputAvailable <- hWaitForInput stdin timeout
+  unless inputAvailable $ 
+    die $ "Timed out after " ++ show timeout ++ " ms."
 
   message <- getLine
   
@@ -22,3 +43,9 @@ main = do
       ModeEncrypt -> Vigenere.encrypt
       ModeDecrypt -> Vigenere.decrypt
     ) keyword message
+
+parseTimeout :: String -> IO Int
+parseTimeout s = do
+  let timeout = readMaybe s
+  when (isNothing timeout) $ die "Invalid value for timeout"
+  return $ fromJust timeout
