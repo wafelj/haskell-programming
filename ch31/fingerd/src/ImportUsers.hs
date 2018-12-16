56b6cb02903b113a39c06b6a1503797e5942bb25
Florjan Bartol <florjan.bartol@gmail.com>
2018-12-16 06:21:23 +0100

    importusers: implemented argument and file parsing

--- ch31/fingerd/src/ImportUsers.hs
+++ ch31/fingerd/src/ImportUsers.hs
@@ -1,4 +1,17 @@
 module Main where
 
+import System.Environment (getArgs)
+import System.Exit
+
 main :: IO ()
-main = putStrLn "hello"
+main = do
+  args <- getArgs
+  
+  path <- 
+    case args of
+      path:[] -> return path
+      _       -> die "Usage: importusers FILENAME"
+
+  rows <- lines <$> readFile path
+
+  putStrLn $ unlines rows
